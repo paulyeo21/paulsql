fbcb835685352691b729e8d03dda5d0db774248f
Paul Yeo <paulyeo21@gmail.com>
2021-05-30 21:56:45 -0700

    Init cobra boilerplate and server command

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/paulyeo21/paulsql
+
+go 1.15
+
+require github.com/spf13/cobra v1.1.3
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/paulyeo21/paulsql/cmd"
+
+func main() {
+	cmd.Execute()
+}
